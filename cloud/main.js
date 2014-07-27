9f32ff765b74be49459533a10d964bd6316e0a68
Jose Montes de Oca <jfmontesdeoca11@gmail.com>
2014-07-26 17:39:25 -0700

    Update user totalCash after transaction save

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,26 @@
+// Updating User object after a Transaction save operation
+Parse.Cloud.afterSave("Transaction", function(request) {
+  transaction = request.object;
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+  query = new Parse.Query(Parse.User);
+  query.get(transaction.get("user").id, {
+    success: function(user) {
+      if (transaction.get("type") == 1) {
+        // Debit transaction, increase cash
+        user.increment("totalCash", transaction.get("amount"));
+      } else {
+        // Credit transaction, decrease cash
+        if (!user.get("totalCash")) {
+          totalCash = 0;
+        } else {
+          totalCash = user.get("totalCash");
+        }
+        user.set("totalCash", totalCash - transaction.get("amount"))
+      }
+      user.save();
+    },
+    error: function(error) {
+      console.error("Got an error " + error.code + " : " + error.message);
+    }
+  });
 });
